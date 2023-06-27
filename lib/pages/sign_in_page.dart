7e52fa182714f28afbe8dab76c3fed28f4fe196d
Guillaume Merat <guillaume.merat@campus-la-chataigneraie.org>
2023-06-27 17:07:15 +0200

    create sign in page

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -1,48 +1,68 @@
 import 'package:flutter/material.dart';
-import 'package:room_manager/pages/sign_in_page.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class SignInPage extends StatefulWidget {
+  const SignInPage({Key? key}) : super(key: key);
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<SignInPage> createState() => _SignInPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _SignInPageState extends State<SignInPage> {
   final _formKey = GlobalKey<FormState>();
 
+  final nameController = TextEditingController();
   final mailController = TextEditingController();
   final passwordController = TextEditingController();
+  final repeatPasswordController = TextEditingController();
 
   @override
   void dispose() {
     super.dispose();
+    nameController.dispose();
     mailController.dispose();
     passwordController.dispose();
+    repeatPasswordController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-        title: Text("Connexion"),
+      appBar: AppBar(
+        title: Text("Inscription"),
       ),
       body: Container(
         margin: EdgeInsets.all(20),
         child: Column(
           children: [
-            const Text(
-              "Connectez-vous",
+            Text(
+              "Inscrivez-vous",
               style: TextStyle(
                 fontSize: 28,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 30),
+            SizedBox(height: 40),
             Form(
               key: _formKey,
               child: Column(
                 children: [
+                  Container(
+                    margin: EdgeInsets.only(bottom: 10),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Nom Prénom',
+                        hintText: "Entrez votre nom et votre prénom",
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Renseignez votre nom et prénom";
+                        }
+                        return null;
+                      },
+                      controller: nameController,
+                    ),
+                  ),
                   Container(
                     margin: EdgeInsets.only(bottom: 10),
                     child: TextFormField(
@@ -78,40 +98,48 @@ class _LoginPageState extends State<LoginPage> {
                       controller: passwordController,
                     ),
                   ),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 10),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Confirmez votre mot de passe',
+                        hintText: "Mot de passe",
+                        border: OutlineInputBorder(),
+                      ),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Renseignez votre mot de passe";
+                        }
+                        if (value != passwordController.text) {
+                          return "Les mots de passe ne correspondent pas";
+                        }
+                        return null;
+                      },
+                      controller: repeatPasswordController,
+                    ),
+                  ),
                   SizedBox(
                     width: double.infinity,
                     height: 50,
                     child: ElevatedButton(
                       onPressed: () {
                         if (_formKey.currentState!.validate()) {
+                          final name = nameController.text;
                           final mail = mailController.text;
                           final password = passwordController.text;
+                          final repeatPassword =
+                              repeatPasswordController.text;
                           ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(content: Text("Connexion...")),
+                            const SnackBar(content: Text("Inscription...")),
                           );
                           FocusScope.of(context).requestFocus(FocusNode());
-                          print("Connexion de $mail avec le mdp $password");
+                          print("Inscription de $name");
+                          print("Avec l'email : $mail");
+                          print("Et le mdp : $password et $repeatPassword");
                         }
                       },
-                      child: Text("Se connecter"),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 40),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (_) => SignInPage()),
-                        );
-                      },
-                      child: const Text(
-                        "Vous n'avez pas de compte ? S'inscrire ici",
-                        style: TextStyle(
-                          color: Colors.blue,
-                          decoration: TextDecoration.underline,
-                        ),
-                      ),
+                      child: Text("S'inscrire"),
                     ),
                   ),
                 ],
@@ -120,6 +148,7 @@ class _LoginPageState extends State<LoginPage> {
           ],
         ),
       ),
+      resizeToAvoidBottomInset: false,
     );
   }
 }
